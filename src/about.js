94a25126b95e2668c6cdcbc79d43af75324db883
DaniellaOhenewaaAsumadu <you@example.com>
2025-01-12 10:03:51 +0000

    adding new file

---
+++ src/about.js
@@ -0,0 +1,276 @@
+// function show (){
+//     let or =document.getElementById("hey");
+//     or.style.width="50%"
+//     or.style.marginLeft="300px"
+//     or.style.transitionDuration="300ms";
+    // let name =prompt("what is your name");
+    // let mon =document.getElementById("am");
+    // mon.innerHTML=(`Hello ${name}, feel free to navigate this website`);
+    // mon.style.color="white";
+// }
+// let more = document.getElementById("hey");
+// more.addEventListener("mouseover",show);
+
+// function Show (){
+//     let Or =document.getElementById("hey2");
+//     Or.style.width="100%"
+//     Or.style.marginLeft="0px"
+// }
+// let More = document.getElementById("hey2");
+// More.addEventListener("mouseleave",Show);
+
+function showtwo (){
+    let or2 =document.getElementById("hey2");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let moretwo = document.getElementById("hey2");
+moretwo.addEventListener("mouseover",showtwo);
+
+
+function Showtwo (){
+    let Or2 =document.getElementById("hey2");
+    Or2.style.width="90%"
+    Or2.style.transitionDuration="300ms";
+
+}
+let Moretwo = document.getElementById("hey2");
+Moretwo.addEventListener("mouseleave",Showtwo);
+
+
+function showthree (){
+    let or2 =document.getElementById("hey3");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let morethree = document.getElementById("hey3");
+morethree.addEventListener("mouseover",showthree);
+
+
+function Showthree (){
+    let Or3 =document.getElementById("hey3");
+    Or3.style.width="90%"
+    Or3.style.transitionDuration="300ms";
+
+}
+let Morethree = document.getElementById("hey3");
+Morethree.addEventListener("mouseleave",Showthree);
+
+
+
+function showfour (){
+    let or2 =document.getElementById("hey4");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let morefour = document.getElementById("hey4");
+morefour.addEventListener("mouseover",showfour);
+
+
+function Showfour (){
+    let Or4 =document.getElementById("hey4");
+    Or4.style.width="90%"
+    Or4.style.transitionDuration="300ms";
+
+}
+let Morefour = document.getElementById("hey4");
+Morefour.addEventListener("mouseleave",Showfour);
+
+
+function showfive (){
+    let or2 =document.getElementById("hey5");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let morefive = document.getElementById("hey5");
+morefive.addEventListener("mouseover",showfive);
+
+
+function Showfive (){
+    let Or5 =document.getElementById("hey5");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let Morefive = document.getElementById("hey5");
+Morefive.addEventListener("mouseleave",Showfive);
+
+
+function show6 (){
+    let or2 =document.getElementById("hey6");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let more6 = document.getElementById("hey6");
+more6.addEventListener("mouseover",show6);
+
+
+function Show6 (){
+    let Or5 =document.getElementById("hey6");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More6 = document.getElementById("hey6");
+More6.addEventListener("mouseleave",Show6);
+
+function show7 (){
+    let or2 =document.getElementById("hey7");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let more7 = document.getElementById("hey7");
+more7.addEventListener("mouseover",show7);
+
+
+function Show7 (){
+    let Or5 =document.getElementById("hey7");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More7 = document.getElementById("hey7");
+More7.addEventListener("mouseleave",Show7);
+
+function show8 (){
+    let or2 =document.getElementById("hey8");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let more8 = document.getElementById("hey8");
+more8.addEventListener("mouseover",show8);
+
+
+function Show8 (){
+    let Or5 =document.getElementById("hey8");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More8 = document.getElementById("hey8");
+More8.addEventListener("mouseleave",Show8);
+
+function show9 (){
+    let or2 =document.getElementById("hey9");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let more9 = document.getElementById("hey9");
+more9.addEventListener("mouseover",show9);
+
+
+function Show9 (){
+    let Or5 =document.getElementById("hey9");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More9 = document.getElementById("hey9");
+More9.addEventListener("mouseleave",Show9);
+
+function show10 (){
+    let or2 =document.getElementById("hey10");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let more10 = document.getElementById("hey10");
+more10.addEventListener("mouseover",show10);
+
+
+function Show10 (){
+    let Or5 =document.getElementById("hey10");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More10 = document.getElementById("hey10");
+More10.addEventListener("mouseleave",Show10);
+
+function show11 (){
+    let or2 =document.getElementById("hey11");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+}
+
+let more11 = document.getElementById("hey11");
+more11.addEventListener("mouseover",show11);
+
+
+function Show11 (){
+    let Or5 =document.getElementById("hey11");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More11 = document.getElementById("hey11");
+More11.addEventListener("mouseleave",Show11);
+
+function show12 (){
+    let or2 =document.getElementById("hey12");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let more12 = document.getElementById("hey12");
+more12.addEventListener("mouseover",show12);
+
+
+function Show12 (){
+    let Or5 =document.getElementById("hey12");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More12 = document.getElementById("hey12");
+More12.addEventListener("mouseleave",Show12);
+
+function show13 (){
+    let or2 =document.getElementById("hey13");
+    or2.style.width="50%"
+    or2.style.borderradius="30px"
+    or2.style.transitionDuration="300ms";
+   
+}
+
+let more13 = document.getElementById("hey13");
+more13.addEventListener("mouseover",show13);
+
+
+function Show13 (){
+    let Or5 =document.getElementById("hey13");
+    Or5.style.width="90%"
+    Or5.style.transitionDuration="300ms";
+
+}
+let More13 = document.getElementById("hey13");
+More13.addEventListener("mouseleave",Show13);
